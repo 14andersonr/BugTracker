a79c217dde57479ae10c1db6a05c2530bc538207
Richard Anderson III <14andersonr@gmail.com>
2021-02-09 20:43:01 -0500

    Added Error and Project Classes and Initiated File Structure

---
+++ BugTracker.Data/Error.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BugTracker.Data
+{
+    class Error
+    {
+        [Key]
+        public int ErrorId { get; set; }
+
+        [ForeignKey(nameof(Project))] //ForeignKey has a nameof and a public virtual.
+        public int ProjectId { get; set; }
+
+        public virtual Project Project { get; set; }
+
+        [Required]
+        public Guid OwnerId { get; set; }
+
+        [Required]
+        public string Title { get; set; }
+
+        [Required]
+        public bool Resolved { get; set; }
+
+        [Required]
+        public DateTimeOffset CreatedUtc { get; set; }
+
+        public DateTimeOffset? ModifiedUtc { get; set; }
+    }
+}
