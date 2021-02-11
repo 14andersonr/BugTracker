7ec520c69362d5ccab6c6f76a0d520a2f61487f3
Meaza <meazastesfaye@gmail.com>
2021-02-11 17:53:01 -0500

    Comment class finished

---
+++ BugTracker.Data/Comment.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BugTracker.Data
+{
+   public class Comment
+    {
+        [Key]
+      
+        public string Text { get; set; }
+        [Required]
+        public string Title { get; set; }
+        public int CommentId { get; set; }
+        [Required]
+        public DateTimeOffset CreatedUtc { get; set; }
+        public DateTimeOffset ModifiedUtc { get; set; }
+        
+        //[ForeignKey](nameof(Error))]
+
+       // public int? ErrorId {get; set;}
+
+       // public virtual Error Error {get; se;}
+    }
+}
